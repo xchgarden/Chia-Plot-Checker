d6ebb2dab4363300162fc1ccb359097146db6b93
mssc89 <oweziz89@gmail.com>
2021-09-06 18:26:57 +0200

    Fixed plot size, added counting nft/og and listing

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function getPlots(){
 function showAllPlots(){
     for (let plot of plots){
         console.log("Name: "+plot.filename)
-        console.log("Size: "+plot.filename)
+        console.log("Size: "+plot.size)
         console.log("Plot ID: "+plot.plot_id)
         console.log("Pool public key: "+plot.pool_public_key)
         console.log("Pool contract puzzle hash: "+plot.pool_contract_puzzle_hash)
@@ -33,6 +33,31 @@ function showAllPlots(){
 
 function showInfo(){
     console.log("Chia harvester reported "+plots.length+" plots.")
+    let uniquePc = []
+    let nftNum = 0;
+    let ogNum = 0;
+    for (let plot of plots){
+        if(plot.pool_contract_puzzle_hash == null){
+            ogNum = ogNum + 1;
+        }
+        else{
+            nftNum = nftNum + 1;
+            if(!uniquePc.includes(plot.pool_contract_puzzle_hash)){
+                uniquePc.push(plot.pool_contract_puzzle_hash)
+            }
+        } 
+    }
+    console.log(uniquePc.length+" unique pool contract adresses")
+    console.log(nftNum+" NFT plots ("+(nftNum/plots.length*100).toFixed(2)+"%)")
+    console.log(ogNum+" OG plots ("+(ogNum/plots.length*100).toFixed(2)+"%)")
+}
+
+function showAllOgPlots(){
+    for (let plot of plots){
+        if(plot.pool_contract_puzzle_hash == null){
+            console.log(plot.filename)
+        }
+    }
 }
 
 switch (args[0]) {
@@ -42,12 +67,19 @@ switch (args[0]) {
         console.log("")
         console.log("Available options:")
         console.log("-h         Show help")
-        console.log("-a         Show all plots and their info")
+        console.log("-a         Show all plots and all their info")
+        console.log("-og        Show list of all OG plots paths")
         break;
     case '-a':
         plotsPromise = getPlots()
         plotsPromise.then(() => {
             showAllPlots();
+        })
+        break;
+    case '-og':
+        plotsPromise = getPlots()
+        plotsPromise.then(() => {
+            showAllOgPlots();
         })
         break;
     default:
